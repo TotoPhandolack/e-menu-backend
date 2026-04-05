541d620192b6582a341ee90d83c9a6e8d0b1db56
1t0t0 <totophandolack915@gmail.com>
2026-04-05 14:41:11 +0700

    login dto & register dto

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString, IsNotEmpty, IsUUID, MinLength } from 'class-validator';
+
+export class RegisterDto {
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(6)
+    password: string;
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsUUID()
+    restaurant_id: string;
+}
